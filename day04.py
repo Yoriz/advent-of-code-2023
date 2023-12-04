5e4477b53963977d12a4bb5eb2f5010bb1016dfb
Yoriz <dave.wilson1@ntlworld.com>
2023-12-04 22:23:44 +0000

    Update day04.py Part2 now completed

--- day04.py
+++ day04.py
@@ -1,3 +1,4 @@
+import collections
 import typing
 
 TEST_FILENAME = "day4_testdata.txt"
@@ -36,19 +37,45 @@ def part_one() -> int:
     data = yield_data(FILENAME)
     total_score = 0
     for line in data:
-        card_number, winning_numbers, scratch_card_numbers = parse_scratch_card_line(line)
+        card_number, winning_numbers, scratch_card_numbers = parse_scratch_card_line(
+            line
+        )
         score = score_scratch_card(winning_numbers, scratch_card_numbers)
         total_score += score
     return total_score
 
 
+def scratch_card_wins(
+    winning_numbers: list[str], scratch_card_numbers: list[str]
+) -> int:
+    wins = 0
+    for number in scratch_card_numbers:
+        if number in winning_numbers:
+            wins += 1
+    return wins
+
+
 def part_two():
-    return 0
+    data = yield_data(FILENAME)
+    copies = collections.Counter()
+    orignal_amount = 0
+    for number, line in enumerate(data, 1):
+        orignal_amount += 1
+        card_number, winning_numbers, scratch_card_numbers = parse_scratch_card_line(
+            line
+        )
+        wins = scratch_card_wins(winning_numbers, scratch_card_numbers)
+        for copy_number in range(number + 1, number + 1 + wins):
+            copies[copy_number] += 1 + copies[number]
+    total_amount = 0
+    for card_number in range(1, orignal_amount + 1):
+        total_amount += 1 + copies[card_number]
+    return total_amount
 
 
 def main():
     print(f"Part one: {part_one()}")
-    # print(f"Part two: {part_two()}")
+    print(f"Part two: {part_two()}")
 
 
 if __name__ == "__main__":
