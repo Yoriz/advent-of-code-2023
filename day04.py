0ee2908bf947c643bf1cce8e0c2d3a8dcd361a48
Yoriz <dave.wilson1@ntlworld.com>
2023-12-04 20:41:29 +0000

    Create day04.py Part1 completed

---
+++ day04.py
@@ -0,0 +1,55 @@
+import typing
+
+TEST_FILENAME = "day4_testdata.txt"
+FILENAME = "day4_data.txt"
+
+
+def yield_data(filename: str) -> typing.Iterator[str]:
+    with open(file=filename, mode="r") as read_file:
+        for line in read_file:
+            yield line.strip()
+
+
+def parse_scratch_card_line(line: str) -> tuple[str, list[str], list[str]]:
+    line = line.replace("  ", " ")
+    card_number, numbers = line.split(":")
+    numbers1, numbers2 = numbers.strip().split("|")
+    winning_numbers = numbers1.strip().split(" ")
+    scratch_card_numbers = numbers2.strip().split(" ")
+    return card_number, winning_numbers, scratch_card_numbers
+
+
+def score_scratch_card(
+    winning_numbers: list[str], scratch_card_numbers: list[str]
+) -> int:
+    score = 0
+    for number in scratch_card_numbers:
+        if number in winning_numbers:
+            if not score:
+                score = 1
+            else:
+                score *= 2
+    return score
+
+
+def part_one() -> int:
+    data = yield_data(FILENAME)
+    total_score = 0
+    for line in data:
+        card_number, winning_numbers, scratch_card_numbers = parse_scratch_card_line(line)
+        score = score_scratch_card(winning_numbers, scratch_card_numbers)
+        total_score += score
+    return total_score
+
+
+def part_two():
+    return 0
+
+
+def main():
+    print(f"Part one: {part_one()}")
+    # print(f"Part two: {part_two()}")
+
+
+if __name__ == "__main__":
+    main()
