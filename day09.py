e0f186e50dfdc5fbde038853c5c7748c887174e7
Yoriz <dave.wilson1@ntlworld.com>
2023-12-09 14:29:03 +0000

    Update day09.py Part2 solved

--- day09.py
+++ day09.py
@@ -1,8 +1,9 @@
+import collections
 import dataclasses
 import typing
 
 TEST_FILENAME = "day9_testdata.txt"
-FILENAME = "day9_data.txt"
+FILENAME = "year2023\day9_data.txt"
 
 
 def yield_data(filename: str) -> typing.Iterator[str]:
@@ -11,21 +12,37 @@ def yield_data(filename: str) -> typing.Iterator[str]:
             yield line.strip()
 
 
+def yield_pairs(values: collections.deque, backwards: bool = False):
+    if backwards:
+        values = values.copy()
+        values.reverse()
+    new_values = iter(values)
+    first = next(new_values)
+    for next_ in new_values:
+        yield first, next_
+        first = next_
+
+
 @dataclasses.dataclass
 class Sequence:
-    values: list[int] = dataclasses.field(default_factory=list)
-    values_history: list[list[int]] = dataclasses.field(default_factory=list)
+    values: collections.deque[int] = dataclasses.field(
+        default_factory=collections.deque
+    )
+    values_history: list[collections.deque[int]] = dataclasses.field(
+        default_factory=list
+    )
 
-    def create_next_values_history(self) -> None:
+    def create_next_values_history(self, backwards: bool = False) -> None:
         if self.values_history:
             values = self.values_history[-1]
         else:
             values = self.values
-        first_value = values[0]
-        new_values: list[int] = []
-        for second_value in values[1:]:
-            new_values.append(second_value - first_value)
-            first_value = second_value
+        new_values: collections.deque[int] = collections.deque()
+        for first, second in yield_pairs(values, backwards):
+            if backwards:
+                new_values.appendleft(first - second)
+            else:
+                new_values.append(second - first)
         self.values_history.append(new_values)
 
     def last_value_history_is_all_zeros(self) -> bool:
@@ -35,33 +52,41 @@ class Sequence:
                 return False
         return True
 
-    def create_all_values_history(self) -> None:
+    def create_all_values_history(self, backwards: bool = False) -> None:
         loops = 1
         while True:
-            self.create_next_values_history()
+            self.create_next_values_history(backwards)
             if self.last_value_history_is_all_zeros():
                 break
             loops += 1
             if loops // 1000 == 1:
                 print(f"<create_all_values_history> reached {loops} loops")
 
-    def extrapolate_history(self) -> None:
+    def extrapolate_history(self, backwards: bool = False) -> None:
         self.values_history[-1].append(0)
         values_history_length = len(self.values_history) - 1
         for index in range(values_history_length, 0, -1):
             current_values_history = self.values_history[index]
             previous_values_history = self.values_history[index - 1]
-            new_value = previous_values_history[-1] + current_values_history[-1]
-            previous_values_history.append(new_value)
+            if backwards:
+                new_value = previous_values_history[0] - current_values_history[0]
+                previous_values_history.appendleft(new_value)
+            else:
+                new_value = previous_values_history[-1] + current_values_history[-1]
+                previous_values_history.append(new_value)
         first_values_history = self.values_history[0]
-        new_value = self.values[-1] + first_values_history[-1]
-        self.values.append(new_value)
+        if backwards:
+            new_value = self.values[0] - first_values_history[0]
+            self.values.appendleft(new_value)
+        else:
+            new_value = self.values[-1] + first_values_history[-1]
+            self.values.append(new_value)
 
 
 def create_sequences(data: typing.Iterator) -> list[Sequence]:
     sequences: list[Sequence] = []
     for line in data:
-        values = [int(number) for number in line.split(" ")]
+        values = collections.deque(int(number) for number in line.split(" "))
         sequences.append(Sequence(values))
     return sequences
 
@@ -75,15 +100,20 @@ def part_one(sequences: list[Sequence]) -> int:
     return total
 
 
-def part_two() -> int:
-    return 0
+def part_two(sequences: list[Sequence]) -> int:
+    total = 0
+    for sequence in sequences:
+        sequence.create_all_values_history(True)
+        sequence.extrapolate_history(True)
+        total += sequence.values[0]
+    return total
 
 
 def main():
     data = yield_data(FILENAME)
     sequences = create_sequences(data)
     print(f"Part one: {part_one(sequences)}")
-    # print(f"Part two: {part_two()}")
+    print(f"Part two: {part_two(sequences)}")
 
 
 if __name__ == "__main__":
