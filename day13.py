1dd619c1ea420a5567655896b07e618c3eb0ad5f
Yoriz <dave.wilson1@ntlworld.com>
2023-12-13 21:29:33 +0000

    Update day13.py Part2 solved

--- day13.py
+++ day13.py
@@ -1,7 +1,7 @@
+import copy
 import dataclasses
 import enum
 import typing
-
 
 TEST_FILENAME = "day13_testdata.txt"
 FILENAME = "day13_data.txt"
@@ -29,22 +29,6 @@ class Location:
     x: int
     y: int
 
-    # def neighbour_location(self, location_direction: "LocationDirection") -> "Location":
-    #     other_location: "Location" = location_direction.value
-    #     return Location(self.x + other_location.x, self.y + other_location.y)
-
-
-# @enum.unique
-# class LocationDirection(enum.Enum):
-#     UP = Location(0, -1)
-#     UP_RIGHT = Location(1, -1)
-#     RIGHT = Location(1, 0)
-#     DOWN_RIGHT = Location(1, 1)
-#     DOWN = Location(0, 1)
-#     DOWN_LEFT = Location(-1, 1)
-#     LEFT = Location(-1, 0)
-#     UP_LEFT = Location(-1, -1)
-
 
 @dataclasses.dataclass
 class GridLocation:
@@ -68,19 +52,6 @@ class GridLocation:
 class Grid:
     grid_locations: list[list[GridLocation]] = dataclasses.field(default_factory=list)
 
-    # def get_neighbour_grid_location(
-    #     self, grid_location: GridLocation, location_direction: LocationDirection
-    # ) -> GridLocation:
-    #     neighbour_location = grid_location.location.neighbour_location(
-    #         location_direction
-    #     )
-    #     if not self.location_in_grid(neighbour_location):
-    #         return GridLocation(neighbour_location, LocationType.OUT_OF_BOUNDS.value)
-    #     neighbour_grid_location = self.grid_locations[neighbour_location.y][
-    #         neighbour_location.x
-    #     ]
-    #     return neighbour_grid_location
-
     def yield_row(self, y_index) -> typing.Iterator[GridLocation]:
         for x_index in range(self.max_x_location + 1):
             yield self.grid_locations[y_index][x_index]
@@ -93,7 +64,6 @@ class Grid:
         if vertical:
             maximum = self.max_x_location
             command = self.yield_column
-
         else:
             maximum = self.max_y_location
             command = self.yield_row
@@ -124,15 +94,19 @@ class Grid:
 
         return rows
 
-    # def location_in_grid(self, location: Location) -> bool:
-    #     return all(
-    #         (
-    #             location.x > -1,
-    #             location.x < self.max_x_location + 1,
-    #             location.y > -1,
-    #             location.y < self.max_y_location + 1,
-    #         )
-    #     )
+    def yield_smudge_grids(self) -> typing.Iterator[typing.Self]:
+        for y_index in range(self.max_y_location + 1):
+            for x_index in range(self.max_x_location + 1):
+                grid_copy = copy.deepcopy(self)
+                grid_location = grid_copy.grid_locations[y_index][x_index]
+                old_value = grid_location.value
+                new_value = (
+                    LocationType.ROCK.value
+                    if grid_location.type == LocationType.ASH
+                    else LocationType.ASH.value
+                )
+                grid_copy.grid_locations[y_index][x_index].value = new_value
+                yield grid_copy
 
     @property
     def max_x_location(self) -> int:
@@ -191,12 +165,37 @@ def part_one() -> int:
 
 
 def part_two() -> int:
-    return 0
+    data = yield_data(FILENAME)
+    grids = create_grids(data)
+    total = 0
+    for grid in grids:
+        vertical_reflections = grid.get_vertical_left_columns_of_reflection()
+        horizonal_reflections = grid.get_horizontal_above_rows_of_reflection()
+        found_new = False
+        for smudge_grid in grid.yield_smudge_grids():
+            smudge_vertical_reflections = (
+                smudge_grid.get_vertical_left_columns_of_reflection()
+            )
+            smudge_horizonal_reflections = (
+                smudge_grid.get_horizontal_above_rows_of_reflection()
+            )
+            for vertical_reflection in smudge_vertical_reflections:
+                if vertical_reflection not in vertical_reflections:
+                    total += vertical_reflection
+                    found_new = True
+            for horizonal_reflection in smudge_horizonal_reflections:
+                if horizonal_reflection not in horizonal_reflections:
+                    total += horizonal_reflection * 100
+                    found_new = True
+            if found_new:
+                break
+
+    return total
 
 
 def main():
     print(f"Part one: {part_one()}")
-    # print(f"Part two: {part_two()}")
+    print(f"Part two: {part_two()}")
 
 
 if __name__ == "__main__":
